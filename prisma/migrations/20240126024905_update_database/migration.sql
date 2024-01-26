702767b2c5f17d73a34ed2f2ae32eff0ad1108bb
antonio <antonio109mesquita@gmail.com>
2024-01-26 01:28:07 -0300

    fixing bugs/ NextAuth complete, page connect-calendar completed and prisma database 80%

---
+++ prisma/migrations/20240126024905_update_database/migration.sql
@@ -0,0 +1,47 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpires` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `accessToken` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `sessions` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_accounts" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_accounts" ("id", "provider", "provider_account_id", "type", "user_id") SELECT "id", "provider", "provider_account_id", "type", "user_id" FROM "accounts";
+DROP TABLE "accounts";
+ALTER TABLE "new_accounts" RENAME TO "accounts";
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+CREATE TABLE "new_sessions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_sessions" ("expires", "id", "session_token", "user_id") SELECT "expires", "id", "session_token", "user_id" FROM "sessions";
+DROP TABLE "sessions";
+ALTER TABLE "new_sessions" RENAME TO "sessions";
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
